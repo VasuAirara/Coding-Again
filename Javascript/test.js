69ac241873498deb33b7de1941e4463d2ad14695
Vasu Airara <vasu78088@gmail.com>
2026-07-13 23:44:02 +0530

    Simple even numbre double and sum

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,1 +1,9 @@
 console.log("Hi Vasu")
+
+
+
+const data = [1, 2, 3, 4, 5];
+const result = data.filter(n => n % 2 === 0)
+                   .map(n => n * 10)
+                   .reduce((sum, n) => sum + n, 0);
+console.log(`Even numbers doubled and summed: ${result}`);   
